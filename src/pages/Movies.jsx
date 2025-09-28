d12accf160d494eea377e9384c552ff4385b3470
shuddy05 <horlabodehyibrahim@gmail.com>
2025-09-28 15:52:40 +0100

    new commit

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MovieComponent from "../components/MovieComponent";
 const Movies = () => {
   return (
-    <div className="w-full px-4 md:px-8 ">
+    <div className="w-full ">
       <h1 className=" text-[20px] md:text-[32px] mb-[16px] md:mb-[25px] ">
         Movies
       </h1>
